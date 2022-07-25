052593e4c44fa214d5fb0c88f5eef99164106777
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 16:13:22 +0300

    Create PersonManager

---
+++ AccountManagement/AccountManager.Business/Concrete/PersonManager.cs
@@ -0,0 +1,25 @@
+﻿using AccountManager.Data.Abstract;
+using AccountManager.Dto.Concrete;
+using AccountManager.Entity.Concrete;
+using AutoMapper;
+using Core.Utilities.Results;
+
+namespace AccountManager.Business.Concrete
+{
+    public class PersonManager : AsyncBaseManager<PersonDto, Person>
+    {
+        public PersonManager(IPersonDal repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        public override Task<IResult> AddAsync(PersonDto entity)
+        {
+            return base.AddAsync(entity);
+        }
+
+        public override Task<IResult> UpdateAsync(int id, PersonDto entity)
+        {
+            return base.UpdateAsync(id, entity);
+        }
+    }
+}
