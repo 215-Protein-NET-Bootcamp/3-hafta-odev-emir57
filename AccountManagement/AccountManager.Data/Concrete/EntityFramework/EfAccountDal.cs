4a447c1f600c6514233e2066230960e126f1c495
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 14:02:16 +0300

    Create EfPersonDal and EfAccountDal

---
+++ AccountManagement/AccountManager.Data/Concrete/EntityFramework/EfAccountDal.cs
@@ -0,0 +1,10 @@
+﻿using AccountManager.Data.Contexts;
+using AccountManager.Entity.Concrete;
+using Core.DataAccess.EntityFramework;
+using AccountManager.Data.Abstract;
+namespace AccountManager.Data.Concrete.EntityFramework
+{
+    public class EfAccountDal : EfRepositoryBase<Account, AccountManagementDbContext>, IAccountDal
+    {
+    }
+}
