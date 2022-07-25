8614b1cadf9765fc9acccc6f0f722f5428bd69a7
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 15:58:16 +0300

    BusinessMessages

---
+++ AccountManagement/AccountManager.Business/Constants/BusinessMessages.cs
@@ -0,0 +1,21 @@
+﻿namespace AccountManager.Business.Constants
+{
+    public class BusinessMessages
+    {
+        public static readonly string SuccessAdd = "Successfully add";
+        public static readonly string SuccessUpdate = "Successfully update";
+        public static readonly string SuccessDelete = "Successfully delete";
+
+
+        public static readonly string UnSuccessAdd = "UnSuccessfully add";
+        public static readonly string UnSuccessUpdate = "UnSuccessfully update";
+        public static readonly string UnSuccessDelete = "UnSuccessfully delete";
+
+
+        public static readonly string SuccessGet = "Successfully get";
+        public static readonly string UnSuccessGet = "Successfully get";
+
+        public static readonly string SuccessList = "UnSuccessfully list";
+        public static readonly string UnSuccessList = "UnSuccessfully list";
+    }
+}
