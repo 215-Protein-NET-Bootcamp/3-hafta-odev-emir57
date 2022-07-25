2d8a120c11048360df8ebc4bd1c49ec4295fff68
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 16:11:37 +0300

    Create AccountManager

--- AccountManagement/AccountManager.Business/Concrete/AccountManager.cs
+++ AccountManagement/AccountManager.Business/Concrete/AccountManager.cs
@@ -1,6 +1,26 @@
-﻿namespace AccountManager.Business.Concrete
+﻿using AccountManager.Data.Abstract;
+using AccountManager.Dto.Concrete;
+using AccountManager.Entity.Concrete;
+using AutoMapper;
+using Core.DataAccess;
+using Core.Utilities.Results;
+
+namespace AccountManager.Business.Concrete
 {
-    public class AccountManager
+    public class AccountManager : AsyncBaseManager<AccountDto, Account>
     {
+        public AccountManager(IAccountDal repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        public override Task<IResult> AddAsync(AccountDto entity)
+        {
+            return base.AddAsync(entity);
+        }
+
+        public override Task<IResult> UpdateAsync(int id, AccountDto entity)
+        {
+            return base.UpdateAsync(id, entity);
+        }
     }
 }
