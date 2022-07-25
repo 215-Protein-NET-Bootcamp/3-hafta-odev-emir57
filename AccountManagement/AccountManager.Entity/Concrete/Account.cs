77fc95c57ed1d1d2235efe7dab7771854036d216
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 13:35:13 +0300

    Create Account and Person Entity

---
+++ AccountManagement/AccountManager.Entity/Concrete/Account.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountManager.Entity.Concrete
+{
+    public class Account : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime LastActivity { get; set; }
+    }
+}
