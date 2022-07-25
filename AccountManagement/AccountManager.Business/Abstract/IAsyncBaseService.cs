1686c82116965c86dc2e80171be05dfd652d8b5f
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 15:29:03 +0300

    Create IAsyncBaseService

---
+++ AccountManagement/AccountManager.Business/Abstract/IAsyncBaseService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity;
+using Core.Utilities.Results;
+
+namespace AccountManager.Business.Abstract
+{
+    public interface IAsyncBaseService<TDto, TEntity>
+        where TDto : class, IDto, new()
+        where TEntity : class, IEntity, new()
+    {
+        Task<IResult> AddAsync(TEntity entity);
+        Task<IResult> UpdateAsync(TEntity entity);
+        Task<IResult> DeleteAsync(TEntity entity);
+
+        Task<IDataResult<TEntity>> GetByIdAsync(int id);
+        Task<IDataResult<IEnumerable<TEntity>>> GetAllAsync();
+    }
+}
