f026790f9df8b0bab491c5e82b8df7a60381a62d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 13:53:32 +0300

    Create AccountManagementDbContext

---
+++ AccountManagement/AccountManager.Data/Contexts/AccountManagementDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AccountManager.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AccountManager.Data.Contexts
+{
+    public class AccountManagementDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .ToTable("Accounts");
+
+            modelBuilder.Entity<Person>()
+                .ToTable("Persons");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(getConnectionString("SqlServerConnection"));
+        }
+
+        private string getConnectionString(string conName)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            return config.GetConnectionString(conName);
+        }
+    }
+}
