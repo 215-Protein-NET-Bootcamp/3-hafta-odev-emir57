8688697bfb5dface570e530e85a7b8a599420acb
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 16:15:46 +0300

    Create IPersonService and IAccountService

---
+++ AccountManagement/AccountManager.Business/Abstract/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using AccountManager.Dto.Concrete;
+using AccountManager.Entity.Concrete;
+
+namespace AccountManager.Business.Abstract
+{
+    public interface IAccountService : IAsyncBaseService<AccountDto, Account>
+    {
+    }
+}
---
+++ AccountManagement/AccountManager.Business/Abstract/IPersonService.cs
@@ -0,0 +1,9 @@
+﻿using AccountManager.Dto.Concrete;
+using AccountManager.Entity.Concrete;
+
+namespace AccountManager.Business.Abstract
+{
+    public interface IPersonService : IAsyncBaseService<PersonDto, Person>
+    {
+    }
+}
