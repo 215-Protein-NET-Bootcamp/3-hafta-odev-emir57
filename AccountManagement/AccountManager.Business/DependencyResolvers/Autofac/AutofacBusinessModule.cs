95c1ef4ffca9e99ee6e69d218bc5d1995ce32967
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 16:24:35 +0300

    Create AutofacBusinessModule

---
+++ AccountManagement/AccountManager.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using concrete = AccountManager.Business.Concrete;
+using @abstract = AccountManager.Business.Abstract;
+using AccountManager.Data.Concrete.EntityFramework;
+using AccountManager.Data.Abstract;
+
+namespace AccountManager.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            #region Business
+            builder.RegisterType<concrete.AccountManager>().As<@abstract.IAccountService>();
+            builder.RegisterType<concrete.PersonManager>().As<@abstract.IPersonService>();
+            #endregion
+
+            #region DataAccess
+            builder.RegisterType<EfPersonDal>().As<IPersonDal>();
+            builder.RegisterType<EfAccountDal>().As<IAccountDal>();
+            #endregion
+
+        }
+    }
+}
