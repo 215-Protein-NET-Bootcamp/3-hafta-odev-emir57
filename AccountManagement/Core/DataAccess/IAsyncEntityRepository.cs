d8d88994b04efb5754adb3044a7bcb38b3b2f20f
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 13:00:29 +0300

    Create IAsyncEntityRepository

---
+++ AccountManagement/Core/DataAccess/IAsyncEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.DataAccess
+{
+    public interface IAsyncEntityRepository<TEntity>
+    {
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(TEntity entity);
+
+        Task<TEntity> GetAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+    }
+}
