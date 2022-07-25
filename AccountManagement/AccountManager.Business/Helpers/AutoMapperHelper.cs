98a3f81b73a702034ad1bcbc4ed4e1dbc69b96c2
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 15:21:28 +0300

    Business Helper -> AutoMapperHelper

---
+++ AccountManagement/AccountManager.Business/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace AccountManager.Business.Helpers
+{
+    public class AutoMapperHelper : Profile
+    {
+        public AutoMapperHelper()
+        {
+
+        }
+    }
+}
