8df7274f14adbe892b7948a9d3254e9b361bc91b
emir57 <emir.gurbuz06@hotmail.com>
2022-07-25 16:29:57 +0300

    Register AutofacBusinessModule in program.cs

--- AccountManagement/AccountManager.WebApi/Program.cs
+++ AccountManagement/AccountManager.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using AccountManager.Business.DependencyResolvers.Autofac;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region AutofacBusinessModule
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
+#endregion
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
