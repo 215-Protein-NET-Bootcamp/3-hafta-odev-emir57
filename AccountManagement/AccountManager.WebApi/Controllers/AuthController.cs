86bbe69242a2d30b07c79860655ceb17d06269df
emir57 <emir.gurbuz06@hotmail.com>
2022-07-26 15:53:19 +0300

    Login Action

---
+++ AccountManagement/AccountManager.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AccountManager.Business.Abstract;
+using AccountManager.Dto.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManager.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginDto loginDto)
+        {
+            var loginResult = await _authService.LoginAsync(loginDto);
+            if (loginResult.Success == false)
+                return BadRequest(loginResult);
+
+            var tokenResult = await _authService.CreateAccessTokenAsync(loginResult.Data);
+            if (tokenResult.Success == false)
+                return BadRequest(tokenResult);
+            return Ok(tokenResult);
+        }
+    }
+}
