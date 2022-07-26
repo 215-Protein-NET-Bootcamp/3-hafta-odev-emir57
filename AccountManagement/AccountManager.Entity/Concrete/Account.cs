f863f0be7b67fad030f1f8f92b6d7c8c8aaecb39
emir57 <emir.gurbuz06@hotmail.com>
2022-07-26 14:32:57 +0300

    Account and Person entity edit properties

--- AccountManagement/AccountManager.Entity/Concrete/Account.cs
+++ AccountManagement/AccountManager.Entity/Concrete/Account.cs
@@ -8,9 +8,11 @@ namespace AccountManager.Entity.Concrete
         [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime LastActivity { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
--- AccountManagement/AccountManager.Entity/Concrete/Person.cs
+++ AccountManagement/AccountManager.Entity/Concrete/Person.cs
@@ -14,6 +14,7 @@ namespace AccountManager.Entity.Concrete
         public string Description { get; set; }
         public string Phone { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public DateTime? DeletedDate { get; set; }
 
         [NotMapped]
         public string FullName
