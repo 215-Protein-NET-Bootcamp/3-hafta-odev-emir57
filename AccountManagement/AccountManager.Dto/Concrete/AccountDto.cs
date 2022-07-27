bed078e1a2a13f2ea94a99d82aeec3802676e5e0
emir57 <emir.gurbuz06@hotmail.com>
2022-07-27 23:13:13 +0300

    AccountDto -> fix password hash and password salt problem

--- AccountManagement/AccountManager.Dto/Concrete/AccountDto.cs
+++ AccountManagement/AccountManager.Dto/Concrete/AccountDto.cs
@@ -5,8 +5,8 @@ namespace AccountManager.Dto.Concrete
     public class AccountDto : IDto
     {
         public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
     }
