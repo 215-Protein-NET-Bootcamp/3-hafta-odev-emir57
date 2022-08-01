21b1d34080c1020acc392b0b6b2bb7ad6824d7d1
emir57 <emir.gurbuz06@hotmail.com>
2022-08-01 13:41:48 +0300

    delete solution1 folder

--- Solution1/ConsoleApp1/Country.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    public class Country
-    {
-        public string CountryName { get; set; }
-        public string Continent { get; set; }
-        public double Population { get; set; }
-    }
-}
